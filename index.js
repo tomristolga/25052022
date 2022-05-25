5e50c6f09bcbb5974f63797f068d67f019d3d6c8
unknown <tolgatomris1998@gmail.com>
2022-05-25 11:38:14 +0400

    first

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const _ = require('lodash')
+
+const app = express()
+app.use(bodyParser.json())
+
+app.get('/api/env', async (req, res) => {
+
+    const env = process.env
+    const json = JSON.stringify(env)
+    res.send(json)
+})
+
+app.get('/api/hello', async (req, res) => {
+
+    res.send('Hello World')
+})
+
+
+
+app.get('/api/merhaba', (req, res) => {
+
+    res.send('Merhaba Dünya')
+})
+
+app.post('/api/name', (req, res) => {
+
+    const body = _.pick(req.body, ['firstName','lastName'])
+    console.log(body)
+    res.send('Hello '+body.firstName+' '+body.lastName)
+})
+
+app.listen(8080, () => {
+    console.log('app server is running')
+})
